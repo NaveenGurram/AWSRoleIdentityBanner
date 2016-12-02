f807e0a96cc3138f125aefa95bc8cc994b492c63
Naveen Kumar Gurram <gurram.naveen@gmail.com>
2016-12-01 20:07:15 -0500

    rename and updates

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 # TagAwsVpc
-Depending on which VPC you login in AWS console, gets a banner identifying which VPC.
+When working in multiple VPC's with multiple roles, often AWS console doesn't provide a easy way of identifying which role and 
+which VPC we are operating.
+
+This simple chrome extension identifies user information and based on patterns identifies which VPC we are on and provides that
+information as a visual banner to make identification easier.
+
+This plugin runs in background and when you are on [https://console.aws.amazon.com/console/home](https://console.aws.amazon.com/console/home)
+gets the current logged in user details and makes a determination of VPC based on provided rules and displays a banner with your choice of
+css above the Console Menu Bar.
+
+## Screenshots
+
+### When you are on Home page
+
+![Home](https://github.com/NaveenGurram/TagAwsVpc/blob/master/screenshots/Home.png "Home")
+
+### When you are on RDS page
+
+![RDS](https://github.com/NaveenGurram/TagAwsVpc/blob/master/screenshots/RDS.png "RDS")
+
+ 
+
+
 
--- content-dup.js
+++ content-dup.js
@@ -1,4 +1,10 @@
-
+// get configuraton values
+// get values from localsync
+chrome.storage.local.get({
+    cssStyle: "background-color: black;padding: 1px !important;text-align: center;color: red;font-size: 18px;",
+    bannerType: roleName,
+    vpcIdentityParams: "/.(finra-awsdev)/g:DEV;(finra-awsqc):"
+}, function (items) {
     // this is where we get the UserName from the AWS
     var userDetails = $('#nav-usernameMenu > div.nav-elt-label').text();
     //  finra-awsprod
@@ -8,13 +14,11 @@
         env = "PROD"
     } else if (userDetails.indexOf('finra-awsqc') > 0) {
         env = "QC";
-    }else if (userDetails.indexOf('finra-awsdev') > 0) {
-        env = "DEV";
-    } 
+    }
 
-    //<div title="priv_aws_cdip_dev_d/K24425 @ finra-awsdev" class="nav-elt-label">priv_aws_cdip_dev_d/K24425 @ finra-awsdev</div>
+    <div title="priv_aws_cdip_dev_d/K24425 @ finra-awsdev" class="nav-elt-label">priv_aws_cdip_dev_d/K24425 @ finra-awsdev</div>
 
     // now based on env put up a banner.
     $('#awsgnav').before("<div class='identityBanner'> You are in " + env + " VPC <br> You logged in as " + userDetails + "</div>");
-
+});
 
