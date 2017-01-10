1385f5851c750737af3b54458b23c5fa4327cc05
Freeman, Joe <Joe.Freeman@finra.org>
2017-01-10 08:36:37 -0500

    Break out priv from user id in federated account

--- awsIdentityBanner.js
+++ awsIdentityBanner.js
@@ -6,13 +6,29 @@ if ($('#awsc-login-display-name-account').length) {
     if ($('#awsc-login-display-name-user').length) {
         userName = $('#awsc-login-display-name-user').text();
     }
+    if (userName.indexOf('/')){
+        $('#awsgnav').before("<div id='identityBanner' class='identityBanner'> Account: <strong>" 
+            + federatedAccount 
+            + "</strong>. Priv: <strong><underline>" 
+            + userName.substring(0,userName.indexOf('/')) 
+            + "</strong>. Userid: <strong><underline>" 
+            + userName.substring(userName.indexOf('/')+1) 
+            + "</underline></strong></div>");
+    } else {
     // now based on env put up a banner.
-    $('#awsgnav').before("<div id='identityBanner' class='identityBanner'> Federated Account: <strong>" + federatedAccount + "</strong>. Userid: <strong><underline>" + userName + "</underline></strong></div>");
+        $('#awsgnav').before("<div id='identityBanner' class='identityBanner'> Federated Account: <strong>" 
+            + federatedAccount 
+            + "</strong>. Userid: <strong><underline>" 
+            + userName 
+            + "</underline></strong></div>");
+    }
 } else if ($('#nav-usernameMenu > div.nav-elt-label').length) {
     // this is where we get the UserName from the AWS
     userName = $('#nav-usernameMenu > div.nav-elt-label').text();
     // now based on env put up a banner.
-    $('#awsgnav').before("<div id='identityBanner' class='identityBanner'> Non-Federated: Userid: <i>" + userName + "</i></div>");
+    $('#awsgnav').before("<div id='identityBanner' class='identityBanner'> Non-Federated: Userid: <i>" 
+        + userName 
+        + "</i></div>");
 } else {
     $('#awsgnav').before("<div id='identityBanner' class='identityBanner'> AWSRoleIdnetityBanner  unable to determine account info.</div>");
 }
